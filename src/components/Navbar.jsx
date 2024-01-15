8f8bf10a192f31d929876107c245923cae6a7008
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2024-01-15 22:55:35 +0200

    Add active link styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
-import { Link } from "react-router-dom";
+import { NavLink as Link } from "react-router-dom";
 
 const Navbar = () => {
+  const activeLinkStyles = ({ isActive }) =>
+    isActive ? "underline underline-offset-4" : "hover:text-sky-500";
+
   return (
-    <nav className="h-20 w-full shadow-md flex items-center px-8 gap-4 text-lg font-semibold">
-      <Link to="/" className="hover:text-sky-500">
+    <nav className="h-20 w-full shadow-md flex items-center px-8 gap-4 text-lg font-semibold ">
+      <Link to="/" className={activeLinkStyles}>
         Home
       </Link>
-      <Link to="/about" className="hover:text-sky-500">
+      <Link to="/about" className={activeLinkStyles}>
         About
       </Link>
     </nav>
