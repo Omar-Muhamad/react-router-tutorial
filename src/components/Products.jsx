67a7dd1f9f85e9b9a76914186682ff8a82a74758
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2024-01-16 00:07:08 +0200

    -

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -9,6 +9,7 @@ const Products = () => {
         placeholder="Search products"
         className="border-2 p-2"
       />
+      <h1>Nested Routes</h1>
       <nav className="text-lg font-semibold flex gap-2">
         <Link to="featured" className={activeLinkStyles}>Featured</Link>
         <Link to="new" className={activeLinkStyles}>New</Link>
